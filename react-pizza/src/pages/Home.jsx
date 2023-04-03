bf79a8579c4c516907981a6396f6dddaf9ef7138
Andrii Nikulin <nikulin.and90@gmail.com>
2023-04-03 23:09:16 +0300

    new pages

---
+++ react-pizza/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/skeleton';
+
+ const Home = () => {
+
+    const [items, setItems] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+  
+    React.useEffect(() =>{
+      fetch('https://6413417ea68505ea732e44de.mockapi.io/items')
+    .then((res) =>  res.json())
+    .then((arr) => {
+      setItems(arr);
+      setIsLoading(false);
+    })
+    }, []);
+    return (
+        <>
+             <div className="content__top">
+              <Categories/>
+              <Sort/>
+            
+                  
+                </div>
+                <h2 className="content__title">Все пиццы</h2>
+                <div className="content__items">
+                
+                {isLoading
+                ?[...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+                : items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)}
+
+          </div>
+        </>
+    )
+}
+
+export default Home;
