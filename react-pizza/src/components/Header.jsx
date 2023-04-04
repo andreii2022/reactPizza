9564703aa960288aae4316cb4a63340128ced871
Andrii Nikulin <nikulin.and90@gmail.com>
2023-04-04 16:04:59 +0300

    add page Cart

--- react-pizza/src/components/Header.jsx
+++ react-pizza/src/components/Header.jsx
@@ -1,18 +1,22 @@
-import LogoSvg from '../assets/img/pizza-logo.svg'
+import LogoSvg from '../assets/img/pizza-logo.svg';
+import { Link } from 'react-router-dom';
 
 function Header () {
     return(
     <div class="header">
     <div class="container">
-      <div class="header__logo">
+     <Link to="/">
+     <div class="header__logo">
         <img width="38" src={LogoSvg} alt="Pizza logo" />
         <div>
           <h1>React Pizza</h1>
           <p>самая вкусная пицца во вселенной</p>
         </div>
+     
       </div>
-      <div className="header__cart">
-        <a href="/cart.html" class="button button--cart">
+      </Link> 
+        <div className="header__cart">
+        <Link to="/cart" class="button button--cart">
           <span>520 ₽</span>
           <div className="button__delimiter"></div>
           <svg
@@ -45,7 +49,7 @@ function Header () {
             />
           </svg>
           <span>3</span>
-        </a>
+        </Link>
       </div>
     </div>
   </div>
--- react-pizza/src/components/NotFoundBlock/index.jsx
+++ react-pizza/src/components/NotFoundBlock/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import styles from './NotFoundBlock.module.scss';
+
 
  const NotFoundBlock = () => {
   return (
     
-    <div>
-        <h1>
+    <>
+        <h1 className={styles.root}>
             <span>:-</span>
             <br />
             Ничего найдено
         </h1>
-    </div>
+        </>
   );
 };
 export default NotFoundBlock;
