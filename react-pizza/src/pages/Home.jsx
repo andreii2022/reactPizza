b3c773e34cbfed586bbde68e38b28e771ec17c21
Andrii Nikulin <nikulin.and90@gmail.com>
2023-04-08 17:36:26 +0300

    home window scrol

--- react-pizza/src/pages/Home.jsx
+++ react-pizza/src/pages/Home.jsx
@@ -17,6 +17,7 @@ import Skeleton from '../components/PizzaBlock/sceleton';
       setItems(arr);
       setIsLoading(false);
     })
+    window.scrollTo(0, 0);
     }, []);
     return (
       <div className="container">
