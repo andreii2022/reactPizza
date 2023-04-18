08772b0cebe6e60013f3c6075f8dc2d097b42909
Andrii Nikulin <nikulin.and90@gmail.com>
2023-04-18 21:54:15 +0300

    redux caunter

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../src/redux/slices/filterSlice'
 
 
 import Header from './components/Header';
@@ -11,16 +13,32 @@ import './scss/app.scss';
 
 export const SearchContext = React.createContext();
 
-
+ 
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
 
   
   return (
          <div className="wrapper">
+
+<button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+            >
+      Increment
+    </button>
+      <span>{count}</span>
+      <button
+      aria-label="Decrement value"
+      onClick={() => dispatch(decrement())}
+      >
+      Decrement   
+ </button>
          
-         <SearchContext.Provider value={{searchValue, setSearchValue}}>
+         {/* <SearchContext.Provider value={{searchValue, setSearchValue}}>
           <Header/>
           <div className="content">
            <Routes>
@@ -30,7 +48,7 @@ function App() {
            </Routes>
         
       </div>
-         </SearchContext.Provider>
+         </SearchContext.Provider> */}
     </div>
   );
 }
